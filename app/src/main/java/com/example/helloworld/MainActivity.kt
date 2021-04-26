36d385bf6dd516b0fa823aafa79dedc702798f1d
chigoRobert <cj4rober@uwaterloo.ca>
2021-04-26 19:12:51 -0400

    updated

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var minus = findViewById<Button>(R.id.decrement_button)
+        var plus = findViewById<Button>(R.id.increment_button)
+        var text = findViewById<TextView>(R.id.counterValue)
+
+        text.text = "" + count
+
+
+        minus.setOnClickListener{
+            counterValue.text = "" + --count
+        }
+
+        plus.setOnClickListener{
+            counterValue.text = "" + ++count
+        }
     }
 }
